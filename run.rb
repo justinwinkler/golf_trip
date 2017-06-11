e5557f2d9078ec0a9c916450266f09e9c14a54d3
Justin Winkler <justinwinkler@google.com>
2017-06-10 22:28:36 -0600

    run.rb cleanup

--- run.rb
+++ run.rb
@@ -3,11 +3,11 @@ require_relative 'lib/data_util.rb'
 
 DataUtil.load(ARGV[0])
 
-payout = Game.get_payout.simplify
+game_payout = Game.get_payout.simplify
 thirty_two_payout = ThirtyTwo.get_payout.simplify
 three_putt_payout = ThreePutt.get_payout.simplify
-payout += thirty_two_payout
-payout += three_putt_payout
+
+payout = game_payout + thirty_two_payout + three_putt_payout
 payout = payout.simplify
 
 Course.print_all
@@ -15,18 +15,17 @@ Round.print_all
 Player.print_all
 Game.print_all
 
+puts "Game Payout".red
+puts game_payout.to_s
+
 puts "32 Payout".red
-thirty_two_payout = ThirtyTwo.get_payout.simplify
-payout += thirty_two_payout
 puts thirty_two_payout.to_s
 
 puts "3-putt Payout".red
-three_putt_payout = ThreePutt.get_payout.simplify
-payout += three_putt_payout
 puts three_putt_payout.to_s
 
 puts "Final Payout".red
-payout = payout.simplify
 puts payout.to_s
+
 puts "Net Winnings".red
 puts payout.net
